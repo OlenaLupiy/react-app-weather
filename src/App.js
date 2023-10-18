4c1e75dfaef89b81d6fcf623afb560301e09fb08
OlenaLupiy <ollupiy@gmail.com>
2023-10-18 14:01:41 +0100

    added GitHub repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This site was coded by Olena Lupiy and is {""}
+        <a href="https://github.com/OlenaLupiy/react-app-weather" target="_blank" rel="noreferrer" >open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
